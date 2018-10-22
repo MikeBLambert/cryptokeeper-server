37a514f2e3adc095531cb16acfb72ab48f5d9b92
MikeBLambert <mlamb128@gmail.com>
2018-10-22 10:37:16 -0700

    Add Account model with associated test that is not yet passing

---
+++ test/unit/account.test.js
@@ -0,0 +1,18 @@
+const Account = require('../../lib/models/Account');
+// const { getErrors } = require('./helpers');
+const { Types } = require('mongoose');
+
+describe('account model', () => {
+    it('validates a good model', () => {
+        const data  = {
+            user: Types.ObjectId(),
+            currencies: [{
+                name: 'BTC',
+                quantity: 5
+            }]
+        };
+        const account = new Account(data);
+        const jsonAccount = account.toJSON();
+        expect(jsonAccount).toEqual({ ...data, _id: expect.any(Object) });
+    });
+});
