f781a6eb12bdc2c62025c5ef872defd047fac9e4
Jack <ajackofsometrades@gmail.com>
2018-10-23 11:24:52 -0700

    holdings post is fixed

--- test/e2e/accounts.test.js
+++ test/e2e/accounts.test.js
@@ -4,73 +4,23 @@ const app = require('../../lib/app');
 const request = require('supertest');
 const Chance = require('chance');
 const chance = new Chance();
-// const { checkStatus } = require('../util/helpers');
+const { checkStatus, signUp, signIn, applyUsers } = require('../util/helpers');
 
-const checkStatus = statusCode => res => {
-    expect(res.status).toEqual(statusCode);
-};
-
-// const users = Array.apply(null, { length: 1 })
-//     .map(() => ({ name: chance.name(), clearPassword: chance.word(), email: chance.email() }));
-
-// const signUp = user => User.create(user);
-
-// const signIn = user => {
-//     return request(app)
-//         .post('/auth/signin')
-//         .send({ email: `${user.email}`, clearPassword: `${user.clearPassword}` })
-//         .then(({ body }) => body.token);
-// };
-
-// describe('account routes', () => {
-    
-//     let createdUsers;
-//     let token;
-
-//     beforeEach(() => {
-//         return (async() => {
-//             await Promise.all([dropCollection('users'), dropCollection('accounts')]);
-//             await Promise.all(users.map(signUp))
-//                 .then(cs => createdUsers = cs);
-//             await signIn(users[0])
-//                 .then(createdToken => token = createdToken);
-//         })();
-//     });
-
-
-const withToken = user => {
-    return request(app)
-        .post('/auth/signin')
-        .send({ email: `${user.email}`, clearPassword: `${user.clearPassword}` })
-        .then(({ body }) => body.token);
-};
+const users = applyUsers();
 
 describe('account routes', () => {
-    const users = Array.apply(null, { length: 1 })
-        .map(() => ({ name: chance.name(), clearPassword: chance.word(), email: chance.email() }));
-
+    
     let createdUsers;
-
-    const createUser = user => {
-        return User.create(user);
-    };
-
-    beforeEach(() => {
-        return dropCollection('users');
-    });
-
-    beforeEach(() => {
-        return Promise.all(users.map(createUser))
-            .then(cs => {
-                createdUsers = cs;
-            });
-    });
-
     let token;
+
     beforeEach(() => {
-        return withToken(users[0]).then(createdToken => {
-            token = createdToken;
-        });
+        return (async() => {
+            await Promise.all([dropCollection('users'), dropCollection('accounts')]);
+            await Promise.all(users.map(signUp))
+                .then(cs => createdUsers = cs);
+            await signIn(users[0])
+                .then(createdToken => token = createdToken);
+        })();
     });
 
     it('creates an account for an authorized user', () => {
@@ -118,7 +68,10 @@ describe('account routes', () => {
                         _id: expect.any(String),
                         user: createdUsers[0]._id.toString(),
                         exchange: account.exchange,
-                        currencies: [holding]
+                        currencies: [{
+                            ...holding,
+                            _id: expect.any(String)
+                        }]
                     });
                 });
         })();
