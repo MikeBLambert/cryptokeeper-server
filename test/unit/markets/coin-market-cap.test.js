cb22f84798db289087cab7c7b5167552f8613578
Jack <ajackofsometrades@gmail.com>
2018-10-23 17:17:58 -0700

    getting top 20 in api

--- test/unit/markets/coin-market-cap.test.js
+++ test/unit/markets/coin-market-cap.test.js
@@ -2,22 +2,32 @@ require('dotenv').config();
 const { getPrices } = require('../../../lib/exchanges/coin-market-cap');
 
 describe('exchange API', () => {
-    it('returns prices for bitcoin', done => {
+    it('runs without error', done => {
         let req = {};
 
         let called = false;
         let error;
+
         const next = err => {
             called = true;
             error = err;
-            
             expect(called).toBeTruthy();
             expect(error).toBeUndefined();
-            expect(req.marketData).toEqual(expect.any(Object));
             done();
         };
-        getPrices(req, null, next);
+        
+        return getPrices(req, null, next);
     });
+    it('returns an array of the top 20 coins', done => {
+        let req = {};
 
+        const next = () => {
+            expect(req.marketData).toHaveLength(20);
+            done();
+        };
+        
+        return getPrices(req, null, next);
+    });
+    
 
 });
