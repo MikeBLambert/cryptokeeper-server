12ee066246ebf33f9c7b3e75d41de542ad7b257e
Claire Flanagan <claireflanagan@gmail.com>
2018-10-24 12:02:24 -0700

    currencies.map test failing exactly how we want it

--- test/unit/utils/get-USD.test.js
+++ test/unit/utils/get-USD.test.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const { getPrices } = require('../../../lib/exchanges/coin-market-cap');
+const { getTotalInUSD } = require('../../../lib/util/get-USD');
 const { dropCollection } = require('../../util/db');
 const app = require('../../../lib/app');
 const request = require('supertest');
@@ -10,7 +11,7 @@ const { checkStatus, signUp, signIn, applyUsers } = require('../../util/helpers'
 describe('get total USD', () => {
     const users = applyUsers(1);
     let createdUsers;
-    let createdAccounts;
+    let createdToken;
     let token;
 
     beforeEach(async() => {
@@ -31,14 +32,14 @@ describe('get total USD', () => {
             exchange: 'Fake Market',
         };
 
-        let holdingsData = { name: 'BTC', quantity: 12 };
+        let holdingsData = { name: 'Bitcoin', quantity: 12 };
 
         let transactionData = {       
             action: 'buy',
-            currency: 'BTC',
+            currency: 'Bitcoin',
             exchange: 'Fake Market',
             price: chance.natural(),
-            quantity: chance.natural()
+            quantity: 2
         };
 
         await request(app)
@@ -58,9 +59,40 @@ describe('get total USD', () => {
     });
 
     it('takes a user id and market data and returns total value of currencies in USD', () => {
-        // expect(createdUsers).toEqual('dkjfdsh');
-        expect(createdAccounts).toEqual('weeeeeew');
-        
+        let userCurrencies;
+
+        let marketData = [{ 'circulating_supply': 17339437, 
+            'cmc_rank': 1, 
+            'date_added': '2013-04-28T00:00:00.000Z', 
+            'id': 1, 
+            'last_updated': '2018-10-24T16:50:30.000Z', 
+            'max_supply': 21000000, 
+            'name': 'Bitcoin', 
+            'num_market_pairs': 6461, 
+            'quote': { 'USD': { 'last_updated': '2018-10-24T16:50:30.000Z', 
+                'market_cap': 112694764843.75227, 
+                'percent_change_1h': 0.180097, 
+                'percent_change_24h': 0.498449, 
+                'percent_change_7d': -0.600313, 
+                'price': 6499.33240876, 
+                'volume_24h': 3429219996.76878 } }, 
+            'slug': 'bitcoin', 
+            'symbol': 'BTC', 
+            'total_supply': 17339437 
+        }];
+
+
+        return request(app)
+            .get('/users/accounts/anyid')
+            .set('Authorization', `Bearer ${token}`) 
+            .then(res => {
+                userCurrencies = res.body.currencies;
+                const totalCurrencies = getTotalInUSD(userCurrencies, marketData);
+                expect(totalCurrencies).toEqual('dfgdfd');
+
+            });           
+            
+
 
     });
 });
