e6dd3e08f5473cf2fb8e3055cf747f25405e73b3
Jack <ajackofsometrades@gmail.com>
2018-10-24 10:05:58 -0700

    connecting to mongo in staging environment

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
-require('./lib/util/connect');
+const connect = require('./lib/util/connect');
 
 const { createServer } = require('http');
 const app = require('./lib/app');
 
 const port = 9876;
 
+connect(process.env.MONGODB_URI);
 const server = createServer(app);
 
 server.listen(port, () => {
