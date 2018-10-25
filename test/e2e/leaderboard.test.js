b0a35a52ad3e5affca353b5da7e386ab8d6d77b0
mlamb <mlamb128@gmail.com>
2018-10-24 20:45:01 -0700

    Leaderboard static and route passing

--- test/e2e/leaderboard.test.js
+++ test/e2e/leaderboard.test.js
@@ -10,7 +10,7 @@ const { checkStatus, signUp, signIn, applyUsers } = require('../util/helpers');
 
 describe('leaderboard', () => {
 
-    const userTemplates = applyUsers(10);
+    const userTemplates = applyUsers(15);
     let createdUsers;
     let createdTokens;
 
@@ -66,6 +66,7 @@ describe('leaderboard', () => {
             .then(res => {
                 checkStatus(200)(res);
                 expect(res.body).toHaveLength(10);
+                expect(res.body[0].usd).toBeGreaterThan(res.body[1].usd);
                 res.body.forEach(topUser => {
                     expect(topUser.user).toBeTruthy();
                     expect(topUser.usd).toEqual(expect.any(Number));
@@ -74,55 +75,7 @@ describe('leaderboard', () => {
             });
     });
 });
-  
-    // it('getting the top 10 accounts by account value returns the top 10 by market value', async() => {
-    //     const account = {
-    //         exchange: 'Fake Market',
-    //     };
-
-    //     let createdAccounts;
-
-    //     await Promise.all(createdTokens.map((token) => {
-    //         return request(app)
-    //             .post('/users/accounts')
-    //             .set('Authorization', `Bearer ${token}`)
-    //             .send(account);
-    //     }));
-
-    //     await Promise.all(createdTokens.map((token) => {
-    //         const holding = {
-    //             name: 'BTC',
-    //             quantity: chance.natural()
-    //         };
-    //         return request(app)
-    //             .post('/users/accounts/holdings')
-    //             .set('Authorization', `Bearer ${token}`)
-    //             .send(holding);
-    //     }))
-    //         .then(cs => createdAccounts = cs);
-
-        // set variable here that calls in middleware/util that defines current market value of an account:
-
-        // const topTen = createdAccounts.sort((a, b) => {
-        //     // a.reduce
-        // });
-
-        // await request(app)
-        //     .get('/leaderboard')
-        //     .set('Authorization', `Bearer ${createdTokens[0]}`)
-        //     .then(res => {
-        //         checkStatus(200)(res);
-            // expect(res.body).toEqual({
-            //     _id: expect.any(String),
-            //     user: createdUsers[0]._id.toString(),
-            //     exchange: account.exchange,
-            //     currencies: [{
-            //         ...holding,
-            //     }]
-            // });
-//             });
-//     });
-// });
+ 
 
 
 
