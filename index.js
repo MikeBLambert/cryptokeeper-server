5d279576b7208c255ca4110fffbfd897a9b2ae1e
Jack <ajackofsometrades@gmail.com>
2018-10-24 19:28:42 -0700

    streamer written and ready for testing

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const connect = require('./lib/util/connect');
 const { createServer } = require('http');
 const app = require('./lib/app');
 
+const { startWatch } = require('./lib/streamer/api-watcher');
+startWatch();
+
 const port = process.env.PORT;
 
 connect(process.env.MONGODB_URI);
