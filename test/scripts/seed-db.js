cde2d8f7b20637f1f1586c91a5490949bdd2668e
Claire Flanagan <claireflanagan@gmail.com>
2018-10-26 12:42:34 -0700

    seed data refactor

--- test/scripts/seed-db.js
+++ test/scripts/seed-db.js
@@ -1,80 +1,33 @@
 // const { dropCollection } = require('../util/db');
-const app = require('../../lib/app');
-const request = require('supertest');
+const request = require('superagent');
 const Chance = require('chance');
 const chance = new Chance();
-const { signUp, signIn, applyUsers } = require('../util/helpers');
-const mongoose = require('mongoose');
 
-const connect = require('../../lib/util/connect');
-connect('mongodb://localhost:27017/crypto');
+const HOST = 'http://localhost:9876';
 
-let createdUsers;
-let createdTokens;
-let createdTransactions;
+const symbols = ['BTC', 'XRP', 'ETH', 'LTC'];
 
-const users = applyUsers(15);
-
-const transaction = {
-    currency: 'BTC',
-    exchange: 'Fake Market',
-    price: chance.natural({ min: 1, max: 400 }),
-    quantity: chance.natural({ min: 1, max: 400 })
-};
-
-const dropCollection = (name) => {
-    mongoose.connection.dropCollection(name)
-        .catch(err => {
-            if(err.codeName !== 'NamespaceNotFound') throw err;
-        });
-};
-
-(async() => {
-    await Promise.all([
-        dropCollection('users'),
-        dropCollection('accounts')
-    ]);
-
-    await Promise.all(users.map(signUp))
-        .then(cu => createdUsers = cu);
-
-    await Promise.all(users.map(signIn))
-        .then(tokens => createdTokens = tokens);
-
-
-    await Promise.all(createdTokens.map((token) => {
-        return request(app)
-            .post('/api/users/transactions')
-            .set('Authorization', `Bearer ${token}`)
-            .send(transaction)
-            .then(({ body }) => body); 
-    }))
-        .then((transactions) => createdTransactions = transactions);
-
-        
-    await mongoose.disconnect();
-        
-    await console.table(createdUsers);
-    await console.table(createdTokens);
-    await console.table(createdTransactions);
-        
-    await console.log('test credentials', `email: ${users[0].email}`, `pass: ${users[0].clearPassword}`);
-})();
-    
-// create more users, sign them all in, give each an account, and make them have varying account values
-    
-    
-    
-    
-// await request(app)
-//     .post('/api/users/accounts')
-//     .set('Authorization', `Bearer ${createdTokens[0]}`)
-//     .send(account);
-// await request(app)
-//      .post('/api/users/transactions')
-//      .set('Authorization', `Bearer ${createdTokens[0]}`)
-//      .send(transaction);
-// await request(app)
-//     .get('/api/users/accounts')
-//     .set('Authorization', `Bearer ${createdTokens[0]}`)
-//     .then(({ body }) => createdAccounts = body);  
+Promise.all(Array.apply(null, { length: 20 })
+    .map(() => ({ name: chance.name(), clearPassword: chance.word(), email: chance.email() }))
+    .map(user => {
+        return request
+            .post(`${HOST}/api/auth/signup`)
+            .send({ name: `${user.name}`, email: `${user.email}`, clearPassword: `${user.clearPassword}` })
+            .then(({ body }) => body.token);
+    })
+)
+    .then(tokens => {
+        return Promise.all(tokens.map(token => {
+            return Promise.all(Array.apply(null, { length: 10 })
+                .map((_, i) => {
+                    return request
+                        .post(`${HOST}/api/users/transactions`)
+                        .set('Authorization', `Bearer ${token}`)
+                        .send({    
+                            currency: symbols[i % symbols.length],
+                            exchange: 'Fake Market',
+                            quantity: chance.natural({ min: 1, max: 20 })
+                        });
+                }));
+        }));
+    });
