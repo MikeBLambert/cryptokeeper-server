0d2402fc8073b9c1b9d63a1b92648b8226ef4337
mlamb <mlamb128@gmail.com>
2018-10-28 17:12:10 -0700

    Remove unneeded comments

--- test/util/helpers.js
+++ test/util/helpers.js
@@ -1,4 +1,3 @@
-// const User = require('../../lib/models/User');
 const request = require('supertest');
 const app = require('../../lib/app');
 const Chance = require('chance');
@@ -35,60 +34,10 @@ const signIn = user => {
         .then(({ body }) => body.token);
 };
 
-// class TestHelper {
-//     constructor() {
-//         this.users, this.createdUsers = [];
-//         this.token;
-//     }
-
-//     init(resource, length) {
-//         this[resource] = Array.apply(null, { length: length }).map(() => this.template(`${resource}`));
-//     }
-
-//     template(resource) {
-//         const templates = {
-//             users: {
-//                 name: chance.name(),
-//                 email: chance.email(),
-//                 clearPassword: chance.string({ length: 10 })
-//             }
-//         };
-//         return templates[resource];
-//     }
-
-//     task(resource, data) {
-//         const routes = {
-//             users: '/auth'
-//         };
-//         const route = routes[resource];
-//         return request(app)
-//             .post(route)
-//             .send(data)
-//             .then(res => res.body);
-//     }
-//     taskRunner(resource) {
-//         return Promise.all(this[resource].map(item => this.task(resource, item)))
-//             .then(response => this['created' + resource.replace(/^\w/, c => c.toUpperCase())] = response);
-//     }
-
-//     async wrapper(resource, number) {
-//         await dropCollection(resource);
-//         await this.init(resource, number);
-//         await this.taskRunner(resource);
-//     }
-
-//     assign(collection, source, link) {
-//         this[collection].forEach((item, index) => {
-//             item[link] = this[source][index % 2]._id;
-//         });
-//     }
-// }
-
 module.exports = {
     getErrors,
     checkStatus,
     applyUsers,
     signUp,
     signIn,
-    // TestHelper
 };
